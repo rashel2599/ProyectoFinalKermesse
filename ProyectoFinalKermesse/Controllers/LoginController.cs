c99f1b7d0701c9276f5fb4b40e438437ab675c51
DMarcelo13 <david.saenz8818@est.uca.edu.ni>
2021-10-07 00:35:41 -0600

    Creación de Login

--- ProyectoFinalKermesse/Controllers/LoginController.cs
+++ ProyectoFinalKermesse/Controllers/LoginController.cs
@@ -37,6 +37,8 @@ namespace ProyectoFinalKermesse.Controllers
                     }
 
                     Session["User"] = user;
+                    Session["Usuario"] = user.userName;
+
 
                 }
 
