66620cc929ae3afdaee9d599ef298717ce6269c6
DMarcelo13 <david.saenz8818@est.uca.edu.ni>
2021-10-06 23:53:10 -0600

    Creación de Login

--- ProyectoFinalKermesse/Controllers/LoginController.cs
+++ ProyectoFinalKermesse/Controllers/LoginController.cs
@@ -4,14 +4,50 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace ProyectoFinalKermesse.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
+        }
+        public ActionResult CloseSesion()
+        {
+            Session["User"] = null;
+            return RedirectToAction("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(string User, string Pwd)
+        {
+            try
+            {
+                using (Models.BDKermesseEntities db = new Models.BDKermesseEntities())
+                {
+                    var user = (from d in db.Usuario
+                                 where d.userName == User.Trim() && d.pwd == Pwd.Trim()
+                                 select d).FirstOrDefault();
+                    if (user == null)
+                    {
+                        ViewBag.Error = "Usuario o contraseña invalida";
+                        return View();
+                    }
+
+                    Session["User"] = user;
+
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View();
+            }
+
         }
     }
 }
---
+++ ProyectoFinalKermesse/Filters/VerificarSesion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinalKermesse.Controllers;
+using ProyectoFinalKermesse.Models;
+
+
+namespace ProyectoFinalKermesse.Filters
+{
+    public class VerificarSesion : ActionFilterAttribute
+    {
+        private Usuario user; 
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            try
+            {
+                base.OnActionExecuted(filterContext);
+
+                user = (Usuario)HttpContext.Current.Session["User"];
+
+                if (user == null)
+                {
+
+                    if (filterContext.Controller is LoginController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("~/Login/Login");
+                    }
+
+                }
+
+            }
+            catch (Exception)
+            {
+                filterContext.Result = new RedirectResult("~/Login/Login");
+            }
+
+        }
+
+    }
+}
